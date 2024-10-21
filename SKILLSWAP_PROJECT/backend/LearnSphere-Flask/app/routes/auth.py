82377e3c145cf07db2854ac8c92b7558a98b0ccf
samarth <samarth.patil3101@gmail.com>
2024-10-21 09:22:11 +0530

    Newly updated project.

--- SKILLSWAP_PROJECT/backend/LearnSphere-Flask/app/routes/auth.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from app.models.controllers.auth_controller import register_user, login_user
-
-auth_bp = Blueprint('auth', __name__)
-
-# Define routes
-auth_bp.route('/register', methods=['POST'])(register_user)
-auth_bp.route('/login', methods=['POST'])(login_user)
--- SKILLSWAP_PROJECT/backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'  # Make sure to change 'your_secret_key' in production
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://username:password@localhost/learnsphere_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your_jwt_secret_key'  # Change this in production
-
-    # Additional configuration options can be added here
